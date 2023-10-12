5740a56216f1f322c58cd84db2f0302df16dc8a7
Advaith Vecham <advaithvecham@hotmail.com>
2023-10-12 10:13:26 -0700

    censoring.py satisfies first case and more simcase

---
+++ censoring.py
@@ -0,0 +1,38 @@
+def main():
+    with open('input_output_files\censoring.in', 'r') as f:
+        S = f.readline().strip("\n")
+        censor_string = f.readline()
+        answer_string = ""
+        occurences_list = []
+
+    index = 0
+    while index != len(S):
+        if S[index] == censor_string[0]:
+            #do someting
+            exactment_factor = 0
+            for i in range(len(censor_string)):
+                if censor_string[i] == S[index+i]:
+                    exactment_factor += 1
+                    if exactment_factor == len(censor_string):
+                       S = S[0:index] + S[index+len(censor_string):len(S)]
+                       if len(occurences_list) != 0:
+                            index = go_back(index, occurences_list[len(occurences_list)-1])
+                            occurences_list.pop(len(occurences_list) - 1) 
+                            break
+                       else:
+                           occurences_list=[]
+                else:
+                    occurences_list.append(exactment_factor)
+                    index += i
+                    break
+        else:
+            answer_string += S[index]
+            index += 1
+
+    print(answer_string)
+
+def go_back(index, val):
+    index = index - val
+    return index
+
+main()
